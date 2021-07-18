ab74d59e284d10c9a79328c655fd2d0e3173d6c7
naoya <sn7sn8@gmail.com>
2021-07-18 23:33:54 +0900

    TOP画面の案２

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -2,35 +2,35 @@ export const Top = () => {
   return (
     <div className="fixed w-full bottom-0 px-12 pb-2 lg:pb-10">
       <div className="flex justify-between relative">
-        <div className="flex">
-          <div className="flex flex-col mr-8">
-            <div className="border rounded-full h-8 w-8 mb-2 flex items-center justify-center">
-              ⬆️
-            </div>
-            <div className="border rounded-full h-8 w-8 flex items-center justify-center">
-              ⬇️
-            </div>
-          </div>
-          <div className="">
-            <div>
-              <input
-                type="text"
-                className="rounded-sm px-4 py-2 mb-2 bg-gray-100 w-full border border-gray-300"
-              />
-            </div>
-            <div className="grid grid-cols-2 gap-1 w-64">
-              <div className="flex items-center justify-center bg-green-900 text-xs text-red-500 text-center rounded-md shadow-sm">
-                確定
+        <div className="flex flex-col">
+          <div className="flex items-center justify-evenly w-96 mb-2">
+            <input
+              type="text"
+              className="rounded-sm px-4 py-2 ml-2 w-24 bg-gray-100 border border-gray-300"
+            />
+            <div className="flex items-center">
+              <div className="border rounded-full h-8 w-8 mr-2 flex items-center justify-center">
+                ⬆️
               </div>
-              <div className="flex items-center justify-center bg-green-900 text-xs text-red-500 text-center py-2 rounded-md shadow-sm">
-                取消
+              <div className="border rounded-full h-8 w-8 flex items-center justify-center">
+                ⬇️
               </div>
-              <div className="flex items-center justify-center bg-green-900 text-xs text-red-500 text-center py-2 rounded-md shadow-sm">
+            </div>
+            <div className="flex">
+              <div className="flex items-center justify-center bg-green-900 text-xs px-2 mr-2 text-red-500 text-center rounded-md shadow-sm">
                 役満放銃
               </div>
-              <div className="flex items-center justify-center bg-green-900 text-xs text-red-500 text-center py-2 rounded-md shadow-sm">
+              <div className="flex items-center justify-center bg-green-900 text-xs px-2 text-red-500 text-center py-2 rounded-md shadow-sm">
                 役満ツモられ
               </div>
+            </div>
+          </div>
+          <div className="flex justify-evenly">
+            <div className="flex items-center justify-center bg-green-900 text-xs w-32 text-red-500 text-center py-2 rounded-md shadow-sm">
+              確定
+            </div>
+            <div className="flex items-center justify-center bg-green-900 text-xs w-32 text-red-500 text-center py-2 rounded-md shadow-sm">
+              取消
             </div>
           </div>
         </div>
@@ -45,7 +45,7 @@ export const Top = () => {
           <p>残高</p>
           <p className="text-5xl lg:text-8xl ml-6 tracking-wider">30,000</p>
         </div>
-        <div className="grid grid-cols-2 gap-2 w-80">
+        <div className="grid grid-cols-2 gap-2 w-60 text-sm">
           <div className="bg-green-900 text-red-500 text-center py-2 h-18 rounded-md shadow-sm">
             履歴
           </div>
